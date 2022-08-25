a07c0a12965dc3418d3c36bbe348ac734bef5dae
hsnysf <hasan.lmra@gmail.com>
2022-08-25 22:15:31 +0300

    Add files via upload

---
+++ src/main/java/learn/aws/ecs/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package learn.aws.ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome To AWS ECS";
+	}
+}
